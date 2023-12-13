dce571ef40a0005710334691e00434d0a6d59bb6
ajaytevatia <ajaytevatia@gmail.com>
2023-12-12 20:00:45 -0500

    port changes in docker file

--- fraud_detection_model_api/app/app.py
+++ fraud_detection_model_api/app/app.py
@@ -115,4 +115,4 @@ app = gradio.mount_gradio_app(app, iface, path="/")
 if __name__ == "__main__":
     # Use this for debugging purposes only
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8002)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
